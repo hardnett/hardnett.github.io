db9ff4e225d54d00fa67b0dc3088eda8ce2ff7d7
Charles Hardnett <hardnett@gmail.com>
2019-03-04 22:49:44 -0500

    decent prototype done with search and response in text

--- js/main.js
+++ js/main.js
@@ -1,20 +1,33 @@
 function myFunction() {
-  var input;
+  var searchInput;
 
-  input = document.getElementById('searchInput')
+  searchInput = document.getElementById("searchInput")
 
   const Http = new XMLHttpRequest();
-  const url='https://images-api.nasa.gov/search?q=apollo%2011 &media_type=image';
+  const url="https://images-api.nasa.gov/search?q=" + encodeURIComponent(searchInput.value) + "&media_type=image";
+  console.log(url);
   Http.open("GET", url);
   Http.send();
 
   Http.onreadystatechange=(e)=>{
-    console.log(Http.responseText)
-    obj = JSON.parse(Http.responseText);
+    var processedResponse = escapeSpecialChars(Http.responseText);
+    console.log(processedResponse)
+    obj = JSON.parse(processedResponse);
 
-    document.getElementsByClassName("results").innerHTML = "The total number of hits: " + obj.collection.metadata.total_hits + "\n\n\n";
+    document.getElementById("results").innerHTML = "The total number of hits: " + obj.collection.metadata.total_hits + "<p>";
+    document.getElementById("results").innerHTML += "The total number of items found: " + obj.collection.items.length + "<p>";
     for (var i = 0; i < obj.collection.items.length; i++) {
-      document.getElementById("results").innerHTML += "item # " + i + ": " + obj.collection.items[i].data[0].description + "\n\n";
+      document.getElementById("results").innerHTML += "item # " + i + ": " + obj.collection.items[i].data[0].description + "<br><br>";
     }
   }
+}
+
+/* this function was found here: http://qnimate.com/json-parse-throws-unexpected-token-error-for-valid-json/
+  It cleans up the JSON when the it returns from the API call.
+*/
+function escapeSpecialChars(jsonString) {
+           return jsonString.replace(/\n/g, "\\n")
+               .replace(/\r/g, "\\r")
+               .replace(/\t/g, "\\t")
+               .replace(/\f/g, "\\f");
 }
